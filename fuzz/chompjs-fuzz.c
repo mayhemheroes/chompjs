b0557fbb352e22cb58bcaec25792ed1b0ce40151
ebell495 <ericbell495@gmail.com>
2023-05-27 10:06:40 -0700

    fix: increase input size

--- fuzz/chompjs-fuzz.c
+++ fuzz/chompjs-fuzz.c
@@ -9,13 +9,15 @@ char buf[8196];
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-    memcpy(buf, Data, Size);
-    buf[Size + 1] = 0;
-    struct Lexer lexer;
-    init_lexer(&lexer, buf);
-    while(lexer.lexer_status == CAN_ADVANCE) {
-        advance(&lexer);
+    if (Size > 100) {
+        memcpy(buf, Data, Size);
+        buf[Size + 1] = 0;
+        struct Lexer lexer;
+        init_lexer(&lexer, buf);
+        while(lexer.lexer_status == CAN_ADVANCE) {
+            advance(&lexer);
+        }
+        release_lexer(&lexer);
     }
-    release_lexer(&lexer);
     return 0;
 }
