8a77ddb309c74f50c89720c338d0a7d60cd31777
ebell495 <ericbell495@gmail.com>
2023-05-27 10:19:04 -0700

    fix: correct a buffer overflow

--- fuzz/chompjs-fuzz.c
+++ fuzz/chompjs-fuzz.c
@@ -9,15 +9,20 @@ char buf[8196];
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-    if (Size > 100) {
-        memcpy(buf, Data, Size);
-        buf[Size + 1] = 0;
-        struct Lexer lexer;
-        init_lexer(&lexer, buf);
-        while(lexer.lexer_status == CAN_ADVANCE) {
-            advance(&lexer);
-        }
-        release_lexer(&lexer);
+    memcpy(buf, Data, Size);
+    buf[Size + 1] = 0;
+
+    struct Lexer lexer;
+    init_lexer(&lexer, buf);
+
+    // Force a larger output buffer
+    release_char_buffer(&lexer.output);
+    init_char_buffer(&lexer.output, 8196); 
+
+    while(lexer.lexer_status == CAN_ADVANCE) {
+        advance(&lexer);
     }
+
+    release_lexer(&lexer);
     return 0;
 }
